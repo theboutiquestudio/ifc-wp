3662ed7ccd8682a1e296aebe4f49f3b4690f90d1
yurihs <yurisalvador@hotmail.com>
2017-11-06 16:48:13 -0200

    Mostrar notícias na home

--- site/web/app/themes/ifc-dezessete-tema-main/resources/views/home.blade.php
+++ site/web/app/themes/ifc-dezessete-tema-main/resources/views/home.blade.php
@@ -3,14 +3,16 @@
 @section('content')
   @include('partials.page-header')
 
-  @if (!have_posts())
+  @php($noticias_loop = new WP_Query(['post_type' => 'noticia', 'post_per_page' => 5]))
+
+  @if (!$noticias_loop->have_posts())
     <div class="alert alert-warning">
       {{ __('Sorry, no results were found.', 'sage') }}
     </div>
     {!! get_search_form(false) !!}
   @endif
 
-  @while (have_posts()) @php(the_post())
+  @while ($noticias_loop->have_posts()) @php($noticias_loop->the_post())
     @include('partials.content-'.get_post_type())
   @endwhile
 
