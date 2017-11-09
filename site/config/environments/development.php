788b869b531262defff215749e9e71cdb3b1bc10
yurihs <yurisalvador@hotmail.com>
2017-11-09 12:28:02 -0200

    Enable debug logging

--- site/config/environments/development.php
+++ site/config/environments/development.php
@@ -7,6 +7,7 @@ define('SCRIPT_DEBUG', true);
 //Fix cookies (login wasn't working)
 // define('COOKIE_DOMAIN', '');
 
-// Too many errors! Don't show em.
+// Too many errors! Don't show 'em, just log 'em.
 define('WP_DEBUG_DISPLAY', false);
+define('WP_DEBUG_LOG', true);
 
