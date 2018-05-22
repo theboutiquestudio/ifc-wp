4a6b3763705ba14451e0c1bff7f7014d3074f016
yurihs <yurisalvador@hotmail.com>
2018-05-22 14:34:47 -0300

    Rehabilitar debuggging (desempenho agora está ok)

--- site/config/environments/development.php
+++ site/config/environments/development.php
@@ -1,15 +1,5 @@
 <?php
 /** Development */
-
 define('SAVEQUERIES', true);
 define('WP_DEBUG', true);
 define('SCRIPT_DEBUG', true);
-define('SAVEQUERIES', false);
-
-
-// Temporário: desabilitar debug por razões de desempenho :(
-define('WP_DEBUG', false);
-define('WP_DEBUG_DISPLAY', false);
-define('WP_DEBUG_LOG', false);
-
-@ini_set('display_errors', 0);
