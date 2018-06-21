7f92af0e063a5e43564a80fd857cbe570ba61c4c
jeliel <jelielbraz71@gmail.com>
2018-06-21 13:11:28 -0300

    inserir os campus cadastrados no menu

--- site/web/app/plugins/ifc-funcionalidade/templates/ifc_menu_principal.php
+++ site/web/app/plugins/ifc-funcionalidade/templates/ifc_menu_principal.php
@@ -1,31 +1,23 @@
  <section class="menu-principal" role="navigation">
-    <!-- inicio menu nossos campi  -->
-  <!--   <div class="nossos-campi">
-      <h1 class="sr-only">Nossos Campi</h1>
-      <div class="text-center menu">
-        <div class="title collapsible" href="#collapse-0" data-toggle="collapse"  aria-expanded="false">Nossos <i>Campi</i>&nbsp;&nbsp;<span class="fa fa-caret-right"></span>
-          </div>
-      </div>
-      <ul id="collapse-0" class="list-inline acesso-rapido in">
-        <li class=""><a href="">Abelardo Luz</a></li>
-        <li class=""><a href="">Araquari</a></li>
-        <li class=""><a href="">Blumenau</a></li>
-        <li class=""><a href="">Brusque</a></li>
-        <li class=""><a href="">Camburiú</a></li>
-        <li class=""><a href="">Concórdia</a></li>
-        <li class=""><a href="">Fraiburgo</a></li>
-        <li class=""><a href="">Ibirama</a></li>
-        <li class=""><a href="">Luzerna</a></li>
-        <li class=""><a href="">Rio do Sul</a></li>
-        <li class=""><a href="">Rio do Sul</a></li>
-        <li class=""><a href="">Santa Rosa do Sul</a></li>
-        <li class=""><a href="">São Francisco do Sul</a></li>
-        <li class=""><a href="">Sombrio</a></li>
-
-        <li><a href="">Videira</a></li>
+    <div>
+      <ul>
+        <li>
+          <a>Nossos Campi</a>
+          <ul>
+            <?php
+              $info_campi = IFC_Consulta_campi::get_campi();
+              foreach ($info_campi as $campi) {
+                ?>
+                <li><?php echo($campi->nome); ?></li>
+                <?php
+              }
+            ?>
+          </ul>
+        </li>
       </ul>
+    </div>
 
-    </div> -->
+
     <!-- fim menu nossos campi  -->
     <!-- Menu de Acesso à informaçao -->
       <?php
