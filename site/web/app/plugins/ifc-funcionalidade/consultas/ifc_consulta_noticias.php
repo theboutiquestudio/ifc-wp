08cfcc6087b3f8f69331cb4784cee8ad4bf5986b
yurihs <yurisalvador@hotmail.com>
2018-07-04 13:42:09 -0300

    Adicionar argumento para consultar apenas notícias destacadas

--- site/web/app/plugins/ifc-funcionalidade/consultas/ifc_consulta_noticias.php
+++ site/web/app/plugins/ifc-funcionalidade/consultas/ifc_consulta_noticias.php
@@ -5,22 +5,25 @@ class IFC_Consulta_Noticias {
 	private const POST_TYPE_CAMPUS = 'noticia_campus';
 	private const POST_TYPE_CURSO = 'noticia_curso';
 
-	public static function get_noticias($limite=5){
+	public static function get_noticias($limite=5, $apenas_destaque=false){
 		global $wpdb;
 
 		if (IFC_Func::is_current_site('geral')) {
 			$n_gerais = self::get_noticias_gerais(array(
 				'limite' => $limite,
+				'apenas_destaque' => $apenas_destaque,
 			));
 			return $n_gerais;
 		} else if (IFC_Func::is_current_site('campus')) {
 			$n_gerais = self::get_noticias_gerais(array(
 				'apenas_propagadas' => true,
+				'apenas_destaque' => $apenas_destaque,
 				'limite' => $limite,
 			));
 			$n_campus = self::get_noticias_campus(array(
 				'site_id' => get_network()->id,
 				'blog_id' => get_current_blog_id(),
+				'apenas_destaque' => $apenas_destaque,
 				'limite' => $limite,
 			));
 			return array_merge($n_gerais, $n_campus);
@@ -38,11 +41,13 @@ class IFC_Consulta_Noticias {
 				'site_id' => $campus->site_id,
 				'blog_id' => $campus->blog_id,
 				'apenas_propagadas' => true,
+				'apenas_destaque' => $apenas_destaque,
 				'limite' => $limite,
 			));
 			$n_curso = self::get_noticias_curso(array(
 				'site_id' => get_network()->id,
 				'blog_id' => get_current_blog_id(),
+				'apenas_destaque' => $apenas_destaque,
 				'limite' => $limite
 			));
 			return array_merge($n_campus, $n_curso);
@@ -55,17 +60,31 @@ class IFC_Consulta_Noticias {
 		switch_to_blog($blog_id);
 
 		$apenas_propagadas = array_key_exists('apenas_propagadas', $args) && $args['apenas_propagadas'] === true;
+		$apenas_destaque = array_key_exists('apenas_destaque', $args) && $args['apenas_destaque'] === true;
 
 		$meta_query_propagar = array(
 			'key' => 'propagar_para_todos_os_campi',
 			'value' => '1',
 			'compare' => '='
 		);
+		$meta_query_destaque = array(
+			'key' => 'destaque',
+			'value' => '1',
+			'compare' => '='
+		);
+
+		$meta_queries = array();
+		if ($apenas_propagadas) {
+			array_push($meta_queries, $meta_query_propagar);
+		}
+		if ($apenas_destaque) {
+			array_push($meta_queries, $meta_query_destaque);
+		}
 
 		$query = new WP_Query(array(
 			'post_type' => self::POST_TYPE_GERAL,
 			'posts_per_page' => $args['limite'],
-			'meta_query' => $apenas_propagadas ? array($meta_query_propagar) : null,
+			'meta_query' => $meta_queries,
 		));
 
 		foreach ($query->posts as $post) {
@@ -85,17 +104,31 @@ class IFC_Consulta_Noticias {
 		switch_to_blog($blog_id);
 
 		$apenas_propagadas = array_key_exists('apenas_propagadas', $args) && $args['apenas_propagadas'] === true;
+		$apenas_destaque = array_key_exists('apenas_destaque', $args) && $args['apenas_destaque'] === true;
 
 		$meta_query_propagar = array(
 			'key' => 'propagar_para_todos_os_cursos',
 			'value' => '1',
 			'compare' => '='
 		);
+		$meta_query_destaque = array(
+			'key' => 'destaque',
+			'value' => '1',
+			'compare' => '='
+		);
+
+		$meta_queries = array();
+		if ($apenas_propagadas) {
+			array_push($meta_queries, $meta_query_propagar);
+		}
+		if ($apenas_destaque) {
+			array_push($meta_queries, $meta_query_destaque);
+		}
 
 		$query = new WP_Query(array(
 			'post_type' => self::POST_TYPE_CAMPUS,
 			'posts_per_page' => $args['limite'],
-			'meta_query' => $apenas_propagadas ? array($meta_query_propagar) : null,
+			'meta_query' => $meta_queries,
 		));
 
 		foreach ($query->posts as $post) {
@@ -113,9 +146,23 @@ class IFC_Consulta_Noticias {
 		$blog_id = $args['blog_id'];
 		switch_to_blog($blog_id);
 
+		$apenas_destaque = array_key_exists('apenas_destaque', $args) && $args['apenas_destaque'] === true;
+
+		$meta_query_destaque = array(
+			'key' => 'destaque',
+			'value' => '1',
+			'compare' => '='
+		);
+
+		$meta_queries = array();
+		if ($apenas_destaque) {
+			array_push($meta_queries, $meta_query_destaque);
+		}
+
 		$query = new WP_Query(array(
 			'post_type' => self::POST_TYPE_CURSO,
 			'posts_per_page' => $args['limite'],
+			'meta_query' => $meta_queries,
 		));
 
 		foreach ($query->posts as $post) {
