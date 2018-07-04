cde65b695ef1b3eb2a7a4868afc203d6fa2f28ce
yurihs <yurisalvador@hotmail.com>
2018-07-04 16:13:43 -0300

    Adicionar consulta notícias apenas sem destaque

--- site/web/app/plugins/ifc-funcionalidade/consultas/ifc_consulta_noticias.php
+++ site/web/app/plugins/ifc-funcionalidade/consultas/ifc_consulta_noticias.php
@@ -5,25 +5,28 @@ class IFC_Consulta_Noticias {
 	private const POST_TYPE_CAMPUS = 'noticia_campus';
 	private const POST_TYPE_CURSO = 'noticia_curso';
 
-	public static function get_noticias($limite=5, $apenas_destaque=false){
+	public static function get_noticias($limite=5, $apenas_destaque=false, $apenas_sem_destaque=false){
 		global $wpdb;
 
 		if (IFC_Func::is_current_site('geral')) {
 			$n_gerais = self::get_noticias_gerais(array(
 				'limite' => $limite,
 				'apenas_destaque' => $apenas_destaque,
+				'apenas_sem_destaque' => $apenas_sem_destaque,
 			));
 			return $n_gerais;
 		} else if (IFC_Func::is_current_site('campus')) {
 			$n_gerais = self::get_noticias_gerais(array(
 				'apenas_propagadas' => true,
 				'apenas_destaque' => $apenas_destaque,
+				'apenas_sem_destaque' => $apenas_sem_destaque,
 				'limite' => $limite,
 			));
 			$n_campus = self::get_noticias_campus(array(
 				'site_id' => get_network()->id,
 				'blog_id' => get_current_blog_id(),
 				'apenas_destaque' => $apenas_destaque,
+				'apenas_sem_destaque' => $apenas_sem_destaque,
 				'limite' => $limite,
 			));
 			return array_merge($n_gerais, $n_campus);
@@ -42,12 +45,14 @@ class IFC_Consulta_Noticias {
 				'blog_id' => $campus->blog_id,
 				'apenas_propagadas' => true,
 				'apenas_destaque' => $apenas_destaque,
+				'apenas_sem_destaque' => $apenas_sem_destaque,
 				'limite' => $limite,
 			));
 			$n_curso = self::get_noticias_curso(array(
 				'site_id' => get_network()->id,
 				'blog_id' => get_current_blog_id(),
 				'apenas_destaque' => $apenas_destaque,
+				'apenas_sem_destaque' => $apenas_sem_destaque,
 				'limite' => $limite
 			));
 			return array_merge($n_campus, $n_curso);
@@ -61,6 +66,7 @@ class IFC_Consulta_Noticias {
 
 		$apenas_propagadas = array_key_exists('apenas_propagadas', $args) && $args['apenas_propagadas'] === true;
 		$apenas_destaque = array_key_exists('apenas_destaque', $args) && $args['apenas_destaque'] === true;
+		$apenas_sem_destaque = array_key_exists('apenas_sem_destaque', $args) && $args['apenas_sem_destaque'] === true;
 
 		$meta_query_propagar = array(
 			'key' => 'propagar_para_todos_os_campi',
@@ -72,6 +78,11 @@ class IFC_Consulta_Noticias {
 			'value' => '1',
 			'compare' => '='
 		);
+		$meta_query_sem_destaque = array(
+			'key' => 'destaque',
+			'value' => '1',
+			'compare' => '!='
+		);
 
 		$meta_queries = array();
 		if ($apenas_propagadas) {
@@ -79,6 +90,9 @@ class IFC_Consulta_Noticias {
 		}
 		if ($apenas_destaque) {
 			array_push($meta_queries, $meta_query_destaque);
+		}
+		if ($apenas_sem_destaque) {
+			array_push($meta_queries, $meta_query_sem_destaque);
 		}
 
 		$query = new WP_Query(array(
@@ -105,6 +119,7 @@ class IFC_Consulta_Noticias {
 
 		$apenas_propagadas = array_key_exists('apenas_propagadas', $args) && $args['apenas_propagadas'] === true;
 		$apenas_destaque = array_key_exists('apenas_destaque', $args) && $args['apenas_destaque'] === true;
+		$apenas_sem_destaque = array_key_exists('apenas_sem_destaque', $args) && $args['apenas_sem_destaque'] === true;
 
 		$meta_query_propagar = array(
 			'key' => 'propagar_para_todos_os_cursos',
@@ -116,6 +131,11 @@ class IFC_Consulta_Noticias {
 			'value' => '1',
 			'compare' => '='
 		);
+		$meta_query_sem_destaque = array(
+			'key' => 'destaque',
+			'value' => '1',
+			'compare' => '!='
+		);
 
 		$meta_queries = array();
 		if ($apenas_propagadas) {
@@ -123,6 +143,9 @@ class IFC_Consulta_Noticias {
 		}
 		if ($apenas_destaque) {
 			array_push($meta_queries, $meta_query_destaque);
+		}
+		if ($apenas_sem_destaque) {
+			array_push($meta_queries, $meta_query_sem_destaque);
 		}
 
 		$query = new WP_Query(array(
@@ -147,16 +170,25 @@ class IFC_Consulta_Noticias {
 		switch_to_blog($blog_id);
 
 		$apenas_destaque = array_key_exists('apenas_destaque', $args) && $args['apenas_destaque'] === true;
+		$apenas_sem_destaque = array_key_exists('apenas_sem_destaque', $args) && $args['apenas_sem_destaque'] === true;
 
 		$meta_query_destaque = array(
 			'key' => 'destaque',
 			'value' => '1',
 			'compare' => '='
 		);
+		$meta_query_sem_destaque = array(
+			'key' => 'destaque',
+			'value' => '1',
+			'compare' => '!='
+		);
 
 		$meta_queries = array();
 		if ($apenas_destaque) {
 			array_push($meta_queries, $meta_query_destaque);
+		}
+		if ($apenas_sem_destaque) {
+			array_push($meta_queries, $meta_query_sem_destaque);
 		}
 
 		$query = new WP_Query(array(
