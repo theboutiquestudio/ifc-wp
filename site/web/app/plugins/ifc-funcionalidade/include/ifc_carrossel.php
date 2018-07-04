f62fc19206c0d78632f15a232699af87c78de0b8
yurihs <yurisalvador@hotmail.com>
2018-07-04 16:18:28 -0300

    Não mostrar setas no carrossel quando há apenas uma imagem

--- site/web/app/plugins/ifc-funcionalidade/include/ifc_carrossel.php
+++ site/web/app/plugins/ifc-funcionalidade/include/ifc_carrossel.php
@@ -19,11 +19,12 @@ class IFC_Carrossel {
 			<div class="carrossel__siema">
 				<?php
 				global $post;
+				$mostrar_navegacao = count($posts) > 1;
 				foreach($posts as $post){
 					switch_to_network($post->site_id);
 					switch_to_blog($post->blog_id);
 					setup_postdata($post);
-					self::mostrarPost();
+					self::mostrarPost($mostrar_navegacao);
 				}
 				wp_reset_postdata();
 				restore_current_blog();
@@ -34,19 +35,23 @@ class IFC_Carrossel {
 		<?php
 	}
 
-	private static function mostrarPost(){
+	private static function mostrarPost($navegacao=false){
 		?>
 		<div>
 			<div class="carrossel__slide">
+				<?php if ($navegacao): ?>
 				<a class="carrossel__prev">
 					<span class="dashicons dashicons-arrow-left-alt2"></span>
 				</a>
+				<?php endif; ?>
 				<a class="carrossel__imagem" href="<?php the_permalink(); ?>">
 					<?= wp_get_attachment_image(get_field('thumbnail'), 'full'); ?>
 				</a>
+				<?php if ($navegacao): ?>
 				<a class="carrossel__next">
 					<span class="dashicons dashicons-arrow-right-alt2"></span>
 				</a>
+				<?php endif; ?>
 			</div>
 			<a class="carrossel__titulo" href="<?php the_permalink(); ?>">
 				<?php the_title(); ?>
